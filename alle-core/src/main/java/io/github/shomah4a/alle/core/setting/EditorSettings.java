d2b5ae5af67241e3b21befd13c85d6dda1b82db6
shoma <ca00628@cartahd.com>
2026-04-08 15:28:57 +0000

    EditorSettingsにTAB_WIDTH設定を追加

--- alle-core/src/main/java/io/github/shomah4a/alle/core/setting/EditorSettings.java
+++ alle-core/src/main/java/io/github/shomah4a/alle/core/setting/EditorSettings.java
@@ -13,6 +13,12 @@ public final class EditorSettings {
     public static final Setting<Integer> INDENT_WIDTH = Setting.of("indent-width", Integer.class, 4);
 
     /**
+     * タブ文字の表示幅（タブストップ間隔）。デフォルト8。
+     * タブ文字は次の {@code TAB_WIDTH} の倍数カラムまで空白で展開される。
+     */
+    public static final Setting<Integer> TAB_WIDTH = Setting.of("tab-width", Integer.class, 8);
+
+    /**
      * コメント文字列。デフォルト "# "。
      */
     public static final Setting<String> COMMENT_STRING = Setting.of("comment-string", String.class, "# ");
